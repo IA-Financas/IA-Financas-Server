78711ea39cb8effe46f138e6f1f7b204344532c1
Daniel Lourenço Affonso <86497631+Daniel-online@users.noreply.github.com>
2023-02-22 22:44:30 -0300

    Add files via upload
    extrated versions because didnt unzipped last commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+//experimental hover effect 
+let logoJump= true;
+// movement for animations
+const speed_Movement= 5;
+//functions
+function openRepositories(){
+
+}
+
+function openProjects(){
+
+}
+
+function rollDown_site(){
+
+}
+
+function rollUp_site(){
+
+}
+function modal(){
+  if(PointerEvent= hover){
+  alert= "Inscreva-se no newsletter!"
+  }
+} 
