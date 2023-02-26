6993d686523ecac69cb2c5c86f73e110f8ce9185
Daniel Lourenço Affonso <86497631+Daniel-online@users.noreply.github.com>
2023-02-26 13:26:57 +0000

    The entire html, some Css and some JS

--- index.js
+++ index.js
@@ -23,4 +23,23 @@ function modal(){
   if(PointerEvent= hover){
   alert= "Inscreva-se no newsletter!"
   }
+
 } 
+<script>
+// Get the video
+var video = document.getElementById("Coding Stock Video.mp4");
+
+// Get the button
+var btn = document.getElementById("playButton");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
