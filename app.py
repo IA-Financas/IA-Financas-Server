739f8e17a264000ac2730794fea6810bdaf004a3
Daniel Lourenço Affonso <danielplayeraffonso@gmail.com>
2024-01-10 09:01:26 -0300

    slight alteration in inheritance

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLACLHEMY_DATABASE_URI'] = 'mysql://root:password123@localhost/flask_crud'
-
+@app.route("/base")
+def base():
+    return render_template("base.html")
 @app.route("/")
 def home(): 
-    return render_template("index.html", content="main page")
-
+    return render_template("index.html")
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
