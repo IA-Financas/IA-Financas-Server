74d9533b42c33774716f67c1eb2da63c2a4eb2df
Daniel Lourenço Affonso <danielplayeraffonso@gmail.com>
2024-01-21 13:16:41 -0300

    modifiquei a conexão com o banco de dados (vou ter que modificar de novo depois)

--- app.py
+++ app.py
@@ -5,13 +5,26 @@ from server.random_session_maker import random_string
 from datetime import timedelta
 
 
-
+#configurações do app
 app = Flask(__name__)
 app.permanent_session_lifetime = timedelta(minutes=5)
-app.config['SQLALCHEMY_DATABASE_URI']= ''
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres:123456@localhost/flask_base'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = random_string(length=10)
 
+#ORM -> database
+db = SQLAlchemy(app)
 
+class users(db.Model):
+    id = db.Column("ID", db.Integer, primary_key = True)
+    name = db.Column("NAME", db.String(30))
+    email = db.Column("EMAIL", db.String(30))
+    
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    
+#Rotas
 @app.route("/base")
 def base():
     return render_template("base.html")
@@ -31,8 +44,19 @@ def login():
         user = request.form["name"]
         print(f"Received username: {user}")
         session["user"]= user
+
+        found_users = users.query.filter_by(name="user").first()
+        if found_users:
+            session["email"] = found_users.email
+            flash("Email was saved!")
+        else:
+            if "email" in session:
+                email = session["email"]
+            user = users(user, "")
+            db.session.add(user)
+            db.sessioncommit()
         #flash(f"{user} has been logged sucessfully!", "info")
-        return redirect(url_for("user"))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+        return redirect(url_for("user"), email=email)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
     else:
         if "user" in session:
             flash(f"{user} has been logged sucessfully!","alert")
@@ -56,9 +80,13 @@ def user():
     email= None
     if "user" in session:
         user = session["user"]
+
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_users = users.query.filter_by(name="user").first()
+            found_users.email= email
+            db.commit()
         else:
             if "email" in session:
                 email = session["email"]
@@ -68,9 +96,10 @@ def user():
     else:
         flash(f"You are not logged in", "info")
         return redirect(url_for("login"))
-    
 
 
 
+#Loop da aplicação
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug = True)
